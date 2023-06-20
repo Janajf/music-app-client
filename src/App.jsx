03b149cc4363c4973b3f4c44607284961510a52e
student11 <student11@Codes-MacBook-Air-3.local>
2023-06-20 12:18:26 -0400

    updated client

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import MusicForm from './components/musicForm'
+import Container from "@mui/material/Container";
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <div className="App">
+      <Container>
       <MusicForm/>
+      </Container>
+    
+    </div>
+      
     </>
   )
 }
--- src/components/musicForm.jsx
+++ src/components/musicForm.jsx
@@ -3,32 +3,133 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
-import PersonAddAltOutlinedIcon from "@mui/icons-material/PersonAddAltOutlined";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import axios from "axios";
+import Card from "@mui/material/Card";
+import { CardContent, CardActions } from "@mui/material";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import IconButton from "@mui/material/IconButton";
+import { useState, useEffect } from "react";
+// import env from 'dotenv';
+
 
 const defaultTheme = createTheme();
 
+const CLIENT_ID = "";
+const CLIENT_SECRET = "";
+let searchInput = "";
+
 export default function MusicForm() {
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
+  const theme = useTheme();
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      firstName: data.get("firstName"),
-      lastName: data.get("lastName"),
-      email: data.get("email"),
-      artist: data.get("artist"),
+    let recordData = {
+      record: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        artist: "",
+      },
+    };
+    recordData.record.firstName = data.get("firstName");
+    recordData.record.lastName = data.get("lastName");
+    recordData.record.email = data.get("email");
+    recordData.record.artist = data.get("artist");
+
+    searchInput = data.get("artist");
+
+    // console.log(recordData);
+
+    axios.post(`http://localhost:3000/record`, {
+      "record": {
+                "firstName": recordData.record.firstName,
+                "lastName": recordData.record.lastName,
+                "email": recordData.record.email,
+                "artist": recordData.record.artist
+            }
+
+    }).then(function(response){
+      console.log(response)
+    }).catch(function(error){
+      console.log(error)
     });
+    search();
   };
+
+  useEffect(() => {
+    // API Access Token
+
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    };
+
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => setAccessToken(data.access_token));
+  }, []);
+
+  // Search
+  async function search() {
+    console.log("Searching for " + searchInput); //Brent
+    console.log("Access token " + accessToken);
+
+    // 1. Get request using search to get the Artist ID
+    let searchParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    let artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    console.log("Artist ID is" + artistID);
+
+    // 2. Get request with artist ID to grab all the albums from that artist
+
+    let albumsReturned = await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums" +
+        "?include_groups=album&market=US&limit=50",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAlbums(data.items);
+      });
+    //3. Display those albums to the user
+    console.log(albums);
+  }
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -45,7 +146,7 @@ export default function MusicForm() {
           <Avatar sx={{ m: 1, bgcolor: "success.main" }}>
             <MusicNoteIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component="h1" variant="h4">
             Provide contact info
           </Typography>
           <Box
@@ -97,10 +198,75 @@ export default function MusicForm() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              // onClick={search}
             >
               Search artist
             </Button>
           </Box>
+        </Box>
+      </Container>
+
+      <Container maxWidth="md">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center"
+          }}
+        >
+          <Grid container spacing={2}>
+            {albums.map((album, i) =>{
+              console.log(album);
+              return(
+                <Card sx={{ display: "flex" }}>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" variant="h5">
+                    {album.name}
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                  {album.artists[0].name}
+                  </Typography>
+                </CardContent>
+                <Box
+                  sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
+                >
+                  <IconButton aria-label="previous">
+                    {theme.direction === "rtl" ? (
+                      <SkipNextIcon />
+                    ) : (
+                      <SkipPreviousIcon />
+                    )}
+                  </IconButton>
+                  <IconButton aria-label="play/pause">
+                    <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                  </IconButton>
+                  <IconButton aria-label="next">
+                    {theme.direction === "rtl" ? (
+                      <SkipPreviousIcon />
+                    ) : (
+                      <SkipNextIcon />
+                    )}
+                  </IconButton>
+                </Box>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={album.images[0].url}
+                alt="Live from space album cover"
+              />
+            </Card>
+              )
+            })}
+            
+          </Grid>
         </Box>
       </Container>
     </ThemeProvider>
